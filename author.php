16a44d34da279716468a6bea8876f54651485068
Ryan Duchene <duchenerc@gmail.com>
2014-05-02 14:46:51 -0500

    Added various archive templates (default, category, tag, and author) and associated styling

---
+++ author.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+<div class="c">
+	<h1><?php the_author(); ?></h1>
+	<p class="archive-description"><?php the_author_meta( 'description' ); ?></p>
+	<?php if ( have_posts() ): ?>
+		
+		<?php while ( have_posts() ): the_post(); ?>
+			
+			<section <?php post_class( 'post' ); ?>>
+				<h2><?php the_title(); ?></h2>
+				<div class="post-content">
+					<div class="post-excerpt">
+						<?php the_excerpt(); ?>
+					</div>
+					<ul class="post-meta">
+						<li>
+							<?php the_author_posts_link(); ?>
+						</li>
+						<li>
+							<?php the_time( get_option( 'date_format' ) ); ?>
+						</li>
+						<li>
+							<?php the_category( ', ' ); ?>
+						</li>
+						<?php if ( get_the_tags() ): ?>
+							<li>
+								<?php the_tags( '', ', ', '' ); ?>
+							</li>
+						<?php endif; ?>
+						<li>
+							<a href="<?php the_permalink(); ?>">Continue reading</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+			
+		<?php endwhile; ?>
+		
+	<?php endif; ?>
+</div>
+
+<?php get_footer(); ?>

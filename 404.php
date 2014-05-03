6b9dec47ddeb017c647e5221200085bae094a36f
Ryan Duchene <duchenerc@gmail.com>
2014-05-03 14:23:08 -0500

    Added the 404 template

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="c">
+	<article id="a">
+		<h1>404 Not Found</h1>
+		<p>We're sorry, but we can't seem to find what you're looking for. We sure hope we didn't break anything!</p>
+		<p>Perhaps you should give the search engine a try:</p>
+	</article>
+	<aside id="s">
+		<?php get_sidebar(); ?>
+	</aside>
+</div>
+
+<?php get_footer(); ?>

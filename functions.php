9fda4b6c0e5ee334eca804a81d2ea278b23253bd
Ryan Duchene <duchenerc@gmail.com>
2014-07-19 16:31:24 -0500

    Added setup function for theme Now supports automatic feed links and HTML5 generation

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
 
+if ( ! function_exists( 'electrify_setup' ) )
+{
+	/**
+	* Setup for the Electrify theme.
+	*
+	**/
+	function electrify_setup()
+	{
+		
+		//	Automatic support for adding feeds.
+		//
+		add_theme_support( 'automatic-feed-links' );
+		
+		//	HTML5 generation for elements.
+		//
+		add_theme_support( 'html5', array(
+			'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'
+		) );
+		
+	}
+}
+add_action( 'after_setup_theme', 'electrify_setup' );
+
 /**
  * Includes manager
  */

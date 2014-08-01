39be7b3fa908c3b27aabc19644643ac603212cd2
Ryan Duchene <duchenerc@gmail.com>
2014-07-31 21:08:59 -0500

    Forums now redirect to home if user not logged in

--- bbpress.php
+++ bbpress.php
@@ -1,3 +1,16 @@
+<?php
+
+//	If the user is not logged in, redirect to the homepage.
+//	In a later update, I may call into the Settings API to add
+//	an option to change this from the admin area.
+//
+if ( ! is_user_logged_in() )
+{
+	wp_redirect( home_url(), 302 );
+}
+
+?>
+
 <?php get_header( 'bbpress' ); ?>
 
 <?php the_post(); ?>
